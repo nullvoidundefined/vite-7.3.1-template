75f66c984e2e7b92ed1d9f7faeedf6ee14fbdd61
nullvoidundefined <ian.greenough.developer@gmail.com>
2026-02-17 12:18:37 -0800

    Cleanup template

---
+++ src/components/application/application.tsx
@@ -0,0 +1,15 @@
+import "./application.css";
+
+function Application() {
+  return (
+    <div className="app">
+      <header className="app-header">
+        <h1>Ian Greenough's Demo</h1>
+        <p className="subtitle">A simple starter template</p>
+      </header>
+      <main className="app-main">Your code here...</main>
+    </div>
+  );
+}
+
+export { Application };
